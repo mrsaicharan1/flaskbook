3f53b43225f34befc7d802698e99e3a83e866a4e
Saicharan Reddy <saicharan.reddy1@gmail.com>
2018-01-09 08:08:37 +0000

    implemented WTForms

--- user/forms.py
+++ user/forms.py
@@ -1,11 +1,22 @@
-from flask_wtf import form
-from wtf_form import validators,StringField,PasswordField
-from wtfforms.fields.html5 import Emailfield
+from flask_wtf import Form
+
+from wtforms import validators, StringField, PasswordField
+
+from wtforms.widgets import TextArea
+
+from wtforms.fields.html5 import EmailField
+
+from wtforms.validators import ValidationError
+
+from flask_wtf.file import FileField, FileAllowed
+
+import re
+
 
 class RegisterForm(Form):
     first_name=StringField('First Name',[validators.Required()])
-    last_name=StrngField('Last Name',[validators.Required()])
-    email=EmailField('Email Address',[valiators.Required()])
+    last_name=StringField('Last Name',[validators.Required()])
+    email=EmailField('Email Address',[validators.Required()])
     username=StringField('username',[validators.Required(),validators.length(min=4,max=25)])
     password=PasswordField('New password',[validators.DataRequired(),validators.EqualTo('confirm',message='Passwords must match'),validators.length(min=4,max=25)])
     confirm=PasswordField('Repeat password')
