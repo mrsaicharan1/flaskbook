ddbd1ee5799fe8eda372d9acb874507dc7679452
Saicharan Reddy <saicharan.reddy1@gmail.com>
2018-01-23 09:52:23 +0000

    set up unit testing

---
+++ user/tests.py
@@ -0,0 +1,24 @@
+from application import create_app as create_app_base
+from mongoengine.connection import _get_db
+import unittest
+from flask import session
+
+from user.models import User
+
+class UserTest(unittest.TestCase):
+    def create_app(self):
+        self.db_name = 'flaskbook_test'
+        return create_app_base(
+            MONGODB_SETTINGS={'DB': self.db_name},
+            TESTING=True,
+            WTF_CSRF_ENABLED=False,
+            SECRET_KEY='mySecret',
+            )
+        
+    def setUp(self):
+        self.app_factory = self.create_app()
+        self.app = self.app_factory.test_client()
+
+    def tearDown(self):
+        db = _get_db()
+        db.client.drop_database(db)
